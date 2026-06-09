1b01a0726730451e55fd952207509c07a241fa83
David <davidsatriaf1@gmail.com>
2026-06-09 17:15:23 +0700

    feat: add postgres database and middleware

---
+++ middleware/middleware.go
@@ -0,0 +1,62 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/labstack/echo/v4"
+)
+
+func forbiddenResponse(c echo.Context) error {
+	return c.JSON(http.StatusUnauthorized, map[string]interface{}{
+		"message": http.StatusText(http.StatusUnauthorized),
+	})
+}
+
+func JWTMiddleware(jwtSign string) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			signature := strings.Split(c.Request().Header.Get("Authorization"), " ")
+			if len(signature) < 2 {
+				return forbiddenResponse(c)
+			}
+			if signature[0] != "Bearer" {
+				return forbiddenResponse(c)
+			}
+
+			claim := jwt.MapClaims{}
+			token, err := jwt.ParseWithClaims(signature[1], claim, func(token *jwt.Token) (interface{}, error) {
+				_, ok := token.Method.(*jwt.SigningMethodHMAC)
+				if !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+				}
+				return []byte(jwtSign), nil
+			})
+			if err != nil {
+				return forbiddenResponse(c)
+			}
+
+			method, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok || method != jwt.SigningMethodHS256 {
+				return forbiddenResponse(c)
+			}
+
+			expAt, err := claim.GetExpirationTime()
+			if err != nil {
+				return forbiddenResponse(c)
+			}
+
+			if time.Now().After(expAt.Time) {
+				return forbiddenResponse(c)
+			}
+
+			userIDFloat, _ := claim["id"].(float64)
+			c.Set("id", int(userIDFloat))
+
+			return next(c)
+		}
+	}
+}
