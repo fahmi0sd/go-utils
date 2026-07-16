b57aa71b4431e38eec7d3e45ee24276c7c7c6377
fahmi0sd <davidsatriaf1@gmail.com>
2026-07-16 09:08:11 +0700

    feat: update GetPostgresConnection to use dynamic SSL mode

--- postgres/db.go
+++ postgres/db.go
@@ -10,13 +10,19 @@ import (
 )
 
 func GetPostgresConnection() *gorm.DB {
+	sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "require"
+	}
+
 	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=require",
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
+		sslmode,
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
